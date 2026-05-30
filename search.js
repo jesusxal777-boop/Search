062e79311f165d94c84460db607705c1ceab8883
jesusxal777-boop <jesusxal777@gmail.com>
2026-05-30 15:59:19 -0500

    Update search.js

--- search.js
+++ search.js
@@ -1,7 +1,8 @@
 const SUPABASE_URL = "https://etzdhnpynhgagiwwbqur.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0emRobnB5bmhnYWdpd3dicXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxNTY5NTEsImV4cCI6MjA5NTczMjk1MX0.iaNTV2bv6xWhJ6NlRswklZ04JxjxJlXpndfporV9zCg";
 
-const supabase = window.supabase.createClient(
+const supabase =
+window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
@@ -13,9 +14,11 @@ async function testConnection() {
     .from("pages")
     .select("*");
 
-    console.log("DATA:", data);
-    console.log("ERROR:", error);
-
+    document.body.innerHTML += `
+        <pre>
+${JSON.stringify({ data, error }, null, 2)}
+        </pre>
+    `;
 }
 
 testConnection();
